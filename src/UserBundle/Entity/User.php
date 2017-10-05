419f34546dd35312cdde4c10d817fe620ab3facd
kevin <kpique@telecom-object.com>
2017-10-05 15:39:29 +0200

    Add Databse Connection, Add UserBundle + FOSUserBundle, Remove AppBundle

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="trustscore", type="integer")
+     */
+    private $trustscore;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set trustscore
+     *
+     * @param integer $trustscore
+     *
+     * @return User
+     */
+    public function setTrustscore($trustscore)
+    {
+        $this->trustscore = $trustscore;
+
+        return $this;
+    }
+
+    /**
+     * Get trustscore
+     *
+     * @return int
+     */
+    public function getTrustscore()
+    {
+        return $this->trustscore;
+    }
+}
+
